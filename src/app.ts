f860fcf9feb2f0c7cb789a562ce197878e40e4a3
Nitesh Bagle <niteshbagle43@gmail.com>
2021-12-19 19:12:53 +0530

    custom generic class

--- src/app.ts
+++ src/app.ts
@@ -35,3 +35,30 @@ function extractAndConcat<T extends object, U extends keyof T>(obj: T, key: U) {
 
 console.log(extractAndConcat({ 1: 'a', 2: 'b' }, 2));
 
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string | number>();
+
+textStorage.addItem('Nitesh');
+textStorage.addItem('King');
+textStorage.addItem('Shraddha');
+
+textStorage.removeItem('King')
+
+console.log(textStorage.getItems());
+
+

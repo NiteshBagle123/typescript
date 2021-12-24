755bd841954ad546597398f90e9b3b07e5253aff
Nitesh Bagle <niteshbagle43@gmail.com>
2021-12-24 13:53:32 +0530

    decorator factory example

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,18 @@ function logger(logString: string) {
     }
 }
 
+function withTemplate(template:string, hookId: string) {
+    return (constructor: any) => {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if(hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name;
+        }
+    }
+}
 
-@logger('LOGGING - PERSONDATA')
+@withTemplate('<h1>My Person</h1>', 'app')
 class PersonData {
     name = 'Nitesh';
     constructor() {

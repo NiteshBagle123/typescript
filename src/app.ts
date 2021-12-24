18442275235839dacf608f8bb885452fa90ff918
Nitesh Bagle <niteshbagle43@gmail.com>
2021-12-24 15:38:42 +0530

    wip

--- src/app.ts
+++ src/app.ts
@@ -89,3 +89,33 @@ class Product {
         return this._price * (1 + tax);
     }
 }
+
+interface ValidatorConfig {
+    [property: string]: {
+        [validatorProp: string]: string[]
+    }
+}
+
+const registeredVal: ValidatorConfig = {}
+class Course {
+    title: string;
+    price: number;
+    constructor(t: string, p: number) {
+        this.title = t;
+        this.price = p;
+    }
+}
+
+const courseForm = document.querySelector('form');
+courseForm?.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title') as HTMLInputElement;
+    const priceEl = document.getElementById('price') as HTMLInputElement;
+
+    const title = titleEl.value;
+    const price = +priceEl.value;
+
+    const createdCourse = new Course(title, price);
+    console.log(createdCourse);
+});
+
